543ec94a9b7ec92e9ca0f0b05bc04deb6f721546
ThunderRabbit <zuzazibubabi@gmail.com>
2025-03-22 00:05:44 +0800

    finished header, on to the body

--- app/page.js
+++ app/page.js
@@ -1,11 +1,27 @@
 import Image from "next/image";
 import "./global.css";
 import Header from "@/components/Header";
+import MissionIcon from "@/public/MissionIcon";
 export default function Home() {
   return (
     <>
       <div className="page">
         <Header />
+        <div className="body">
+          <div className="mission-block">
+            <div className="mission-left">
+              <span className="mission-text">
+                Truth First, Awareness Always, Misinformation Never
+              </span>
+              <span className="mission-description">
+                We deliver top economic, legal, and political news while helping
+                users verify information and identify misinformation in the
+                Philippines.
+              </span>
+            </div>
+            <MissionIcon />
+          </div>
+        </div>
       </div>
     </>
   );
