5fc5674827eca864cbbd98972d717fd7c8dd4b44
ThunderRabbit <zuzazibubabi@gmail.com>
2025-03-21 21:21:59 +0800

    made the header responsive

--- public/TalaIcon.jsx
+++ public/TalaIcon.jsx
@@ -6,6 +6,7 @@ export default function TalaIcon() {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className="tala-icon"
     >
       <g id="SVGRepo_bgCarrier" stroke-width="0" />
 
