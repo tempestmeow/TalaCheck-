e4e402b89e8896448d660fb4e9015568eab44d1c
ThunderRabbit <zuzazibubabi@gmail.com>
2025-03-22 19:31:35 +0800

    added use state to toggle the problems, yet to add animations

--- app/page.js
+++ app/page.js
@@ -1,3 +1,7 @@
+"use client";
+
+import React from "react";
+import { useState } from "react";
 import Image from "next/image";
 import "./global.css";
 import Header from "@/components/Header";
@@ -7,7 +11,13 @@ import LearnIcon from "@/public/LearnIcon";
 import FactCheck from "@/public/FactCheck";
 import NewsIcon from "@/public/NewsIcon";
 import NLP from "@/public/NLP";
+import AddIcon from "@/public/AddIcon";
+import SubtractIcon from "@/public/SubtractIcon";
 export default function Home() {
+  const [problemToggle1, setProblemToggle1] = useState(false);
+  const [problemToggle2, setProblemToggle2] = useState(false);
+  const [problemToggle3, setProblemToggle3] = useState(false);
+
   return (
     <>
       <div className="page">
@@ -80,6 +90,131 @@ export default function Home() {
                   </span>
                 </div>
                 <NLP />
+              </div>
+            </div>
+          </div>
+          <div className="problem-block">
+            <div className="problem-header">
+              <span className="problem-title">Key Issues</span>
+              <span className="problem-description">
+                The Need for Media Literacy
+              </span>
+            </div>
+            <div className="problem-list">
+              <div className="problem-item">
+                <div
+                  className={
+                    !problemToggle1
+                      ? "problem-top"
+                      : "problem-top problem-toggled"
+                  }
+                >
+                  <div className="problem-left">
+                    <span className="problem-number">1</span>
+                    <span className="problem-point">
+                      Fake News & Polarization
+                    </span>
+                  </div>
+                  <div className="problem-toggle">
+                    <div className="problem-toggle">
+                      {problemToggle1 ? (
+                        <SubtractIcon
+                          problemToggle={problemToggle1}
+                          setProblemToggle={setProblemToggle1}
+                        />
+                      ) : (
+                        <AddIcon
+                          problemToggle={problemToggle1}
+                          setProblemToggle={setProblemToggle1}
+                        />
+                      )}
+                    </div>
+                  </div>
+                </div>
+                {problemToggle1 && (
+                  <div className="problem-bottom">
+                    Fake news spreads false or misleading information, deepening
+                    political divides. It fuels distrust, reinforces biases, and
+                    manipulates public perception through social media
+                    algorithms and disinformation campaigns.
+                  </div>
+                )}
+              </div>
+              <div className="problem-item">
+                <div
+                  className={
+                    !problemToggle2
+                      ? "problem-top"
+                      : "problem-top problem-toggled"
+                  }
+                >
+                  <div className="problem-left">
+                    <span className="problem-number">2</span>
+                    <span className="problem-point">Impact on Democracy</span>
+                  </div>
+                  <div className="problem-toggle">
+                    <div className="problem-toggle">
+                      {problemToggle2 ? (
+                        <SubtractIcon
+                          problemToggle={problemToggle2}
+                          setProblemToggle={setProblemToggle2}
+                        />
+                      ) : (
+                        <AddIcon
+                          problemToggle={problemToggle2}
+                          setProblemToggle={setProblemToggle2}
+                        />
+                      )}
+                    </div>
+                  </div>
+                </div>
+                {problemToggle2 && (
+                  <div className="problem-bottom">
+                    Fake news spreads false or misleading information, deepening
+                    political divides. It fuels distrust, reinforces biases, and
+                    manipulates public perception through social media
+                    algorithms and disinformation campaigns.
+                  </div>
+                )}
+              </div>
+              <div className="problem-item">
+                <div
+                  className={
+                    !problemToggle3
+                      ? "problem-top"
+                      : "problem-top problem-toggled"
+                  }
+                >
+                  <div className="problem-left">
+                    <span className="problem-number">3</span>
+                    <span className="problem-point">
+                      Media Influence & Bias
+                    </span>
+                  </div>
+                  <div className="problem-toggle">
+                    <div className="problem-toggle">
+                      {problemToggle3 ? (
+                        <SubtractIcon
+                          problemToggle={problemToggle3}
+                          setProblemToggle={setProblemToggle3}
+                        />
+                      ) : (
+                        <AddIcon
+                          problemToggle={problemToggle3}
+                          setProblemToggle={setProblemToggle3}
+                        />
+                      )}
+                    </div>
+                  </div>
+                </div>
+                {problemToggle3 && (
+                  <div className="problem-bottom">
+                    Fake news spreads false or misleading information, deepening
+                    political divides. It fuels distrust, reinforces biases, and
+                    manipulates public perception through social media
+                    algorithms and disinformation campaigns.
+                  </div>
+                )}
               </div>
             </div>
           </div>
